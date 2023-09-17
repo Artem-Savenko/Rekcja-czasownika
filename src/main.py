3dc9fac18836c2bfcf1e84ab9e62570ec1e71516
Artem-Savenko <artemsav4465@gmail.com>
2023-09-17 12:37:41 +0200

    Add: Welcome message

--- src/main.py
+++ src/main.py
@@ -5,6 +5,13 @@ import shlex
 
 allEntries.append(entry.Entry('Myśleć', 'djn', ['Lubię swego kota', 'Może pomyślę nad tym...']))
 
+print('=' * 10 + ' REKCJA CZASOWNIKA ' + '=' * 10 +
+    '\nTen program jest interaktywnym słownikiem, w którym możesz przechowywać rekcje czasowników.'
+	'\nAby wyświetlić wszystkie hasła, wpisz: wylicz'
+	'\nAby pokazać rekcję hasła, wpisz: znajdź <hasło>'
+	'\nAby wyjść z programu, wpisz: wyjdź'
+	'\nAby wyświetlić listę wszystkich komend, wpisz: pomoc\n')
+
 while (True):
     cmd = input('Wpisz komendę: ')
     parser.parseCommand(shlex.split(cmd))
