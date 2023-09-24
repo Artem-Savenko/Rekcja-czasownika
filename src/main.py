021bde5b011ad8941e754662577a468d3eb86ff9
Artem-Savenko <artemsav4465@gmail.com>
2023-09-24 21:15:32 +0200

    Ref: improved whitespaces

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ import shlex
 config.init()  # init global list allEntries
 
 print('=' * 10 + ' REKCJA CZASOWNIKA ' + '=' * 10 +
-    '\nTen program jest interaktywnym słownikiem, w którym możesz przechowywać rekcje czasowników.'
-	'\nAby wyświetlić wszystkie hasła, wpisz: wylicz'
-	'\nAby pokazać rekcję hasła, wpisz: znajdź <hasło>'
-	'\nAby wyjść z programu, wpisz: wyjdź'
-	'\nAby wyświetlić listę wszystkich komend, wpisz: pomoc\n')
+      '\nTen program jest interaktywnym słownikiem, w którym możesz przechowywać rekcje czasowników.'
+      '\nAby wyświetlić wszystkie hasła, wpisz: wylicz'
+      '\nAby pokazać rekcję hasła, wpisz: znajdź <hasło>'
+      '\nAby wyjść z programu, wpisz: wyjdź'
+      '\nAby wyświetlić listę wszystkich komend, wpisz: pomoc\n')
 
 while (True):
     cmd = input('Wpisz komendę: ')
